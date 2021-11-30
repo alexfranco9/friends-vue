9ffbb1496b536a6a9c35fcbdefc1ecb410fc34a8
Alex Franco <alex.franco9@icloud.com>
2021-11-29 23:17:48 -0600

    first commit

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label for="name">Name</label>
+            <input type="text" v-model="enteredName">
+        </div>
+        <div>
+            <label for="phone">Phone</label>
+            <input type="text" v-model="enteredPhone">
+        </div>
+        <div>
+            <label for="email">E-Mail</label>
+            <input type="text" v-model="enteredEmail">
+        </div>
+        <div>
+            <button>Add Contact</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-contact'],
+    data() {
+        return {
+            enteredName: '',
+            enteredPhone: '',
+            enteredEmail: ''
+        };
+    },
+    methods: {
+        submitData() {
+            this.$emit('add-contact', this.enteredName, this.enteredName, this.enteredEmail)
+        }
+    }
+};
+</script>
